52e9ba9b4fcd1411b22e03f7f06c51baba99097a
WANGJIAWEI <WANGJIAWEI@MacBook-Pro.local>
2026-06-19 12:11:28 +0800

    Update to v1.2.34

--- railwise.rb
+++ railwise.rb
@@ -4,21 +4,21 @@
 class Railwise < Formula
   desc "AI coding agent built for the terminal"
   homepage "https://github.com/railwise-cn/RAILWISE-CLI"
-  version "1.2.33"
+  version "1.2.34"
 
   depends_on "ripgrep"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/railwise-cn/RAILWISE-CLI/releases/download/v1.2.33/railwise-darwin-x64.zip"
-      sha256 "dec82b0998d9a78f986852562fa4d3c78fbdb41599c922643d4ff4cb8497f4b6"
+      url "https://github.com/railwise-cn/RAILWISE-CLI/releases/download/v1.2.34/railwise-darwin-x64.zip"
+      sha256 "b55f48859d84083caa3e7b80524e3b9d50993b6a73b67ad4dce707317da8439a"
       def install
         bin.install "railwise"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/railwise-cn/RAILWISE-CLI/releases/download/v1.2.33/railwise-darwin-arm64.zip"
-      sha256 "0f256218325eb06f1aa65899c601b8f1c7f94d9bd8c703fa9520796ab48f7715"
+      url "https://github.com/railwise-cn/RAILWISE-CLI/releases/download/v1.2.34/railwise-darwin-arm64.zip"
+      sha256 "f82285f5df971ce7f10849823de78cd1fbb83ce63195813a7269b5dd027911d2"
       def install
         bin.install "railwise"
       end
@@ -27,15 +27,15 @@ class Railwise < Formula
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/railwise-cn/RAILWISE-CLI/releases/download/v1.2.33/railwise-linux-x64.tar.gz"
-      sha256 "e38350c10115bf7627d942649e9aba4ff5704094f7a2d5cb0df1eae6e1f67efb"
+      url "https://github.com/railwise-cn/RAILWISE-CLI/releases/download/v1.2.34/railwise-linux-x64.tar.gz"
+      sha256 "2297c47800ee6c045829b370028f291e1d9fe47bedc5b10b9e6d0627b5ba1bc2"
       def install
         bin.install "railwise"
       end
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/railwise-cn/RAILWISE-CLI/releases/download/v1.2.33/railwise-linux-arm64.tar.gz"
-      sha256 "cae1573e3160bccbec083d1bbe06da3e616a2e42889f568362030a7384c5ee63"
+      url "https://github.com/railwise-cn/RAILWISE-CLI/releases/download/v1.2.34/railwise-linux-arm64.tar.gz"
+      sha256 "e75f27319fc075f6bd96606cd5226e5c746ee6e1296bac375be2031c35f51a5b"
       def install
         bin.install "railwise"
       end
